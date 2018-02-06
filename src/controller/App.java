a5cef92ec8188236619d5502b38d57bc8a6e7c19
zoltan.bihari <zoltan.bihari@seizo-BZ-acer.seizo.hu>
2018-02-06 12:12:24 +0100

    Sec

--- src/controller/App.java
+++ src/controller/App.java
@@ -7,7 +7,6 @@ import model.Product;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		List<Product> valami = ProductController.getAllProductInList();
 		for (Product product : valami) {
 			System.out.println(product.toString());
@@ -19,12 +18,5 @@ public class App {
 		for (Product product : valami) {
 			System.out.println(product.toString());
 		}
-		//Product p = new Product("Cigi","1100","1012","Cigi") ;
-		/*EntityManagerFactory emf = Persistence.createEntityManagerFactory("Product");
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p);
-		em.getTransaction().commit();
-		em.close();*/
 	}
 }
