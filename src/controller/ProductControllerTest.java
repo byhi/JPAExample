3d3259bcd28e406e7f4665f17ab1eff5654680ae
zoltan.bihari <zoltan.bihari@seizo-BZ-acer.seizo.hu>
2018-02-06 12:19:12 +0100

    AddTest

---
+++ src/controller/ProductControllerTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProductControllerTest {
+
+	@Test
+	public void testGetAllProductInList() {
+		fail("Not yet implemented");
+	}
+
+}
